7c8eae5cc2f1a60dd06acc9369d7f4bda78232a5
colinricardo <colin@colin.fun>
2024-08-28 16:47:31 -0400

    fix: try fix

--- src/app/api/download/route.ts
+++ src/app/api/download/route.ts
@@ -3,13 +3,27 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const { url } = await req.json();
+  const cookies = req.headers.get("cookie") || "";
 
   try {
     const videoId = ytdl.getVideoID(url);
-    const videoInfo = await ytdl.getInfo(videoId);
+    const videoInfo = await ytdl.getInfo(videoId, {
+      requestOptions: {
+        headers: {
+          cookie: cookies,
+        },
+      },
+    });
     const format = ytdl.chooseFormat(videoInfo.formats, { quality: "highest" });
 
-    const videoStream = ytdl(url, { format: format });
+    const videoStream = ytdl(url, {
+      format: format,
+      requestOptions: {
+        headers: {
+          cookie: cookies,
+        },
+      },
+    });
     const chunks: Uint8Array[] = [];
 
     for await (const chunk of videoStream) {
--- src/app/api/preview/route.ts
+++ src/app/api/preview/route.ts
@@ -1,7 +1,7 @@
 import ytdl from "@distube/ytdl-core";
 import { NextRequest, NextResponse } from "next/server";
 
-export default async function POST(req: NextRequest) {
+export const POST = async (req: NextRequest) => {
   try {
     const { url } = await req.json();
     const cookies = req.headers.get("cookie") || "";
@@ -20,7 +20,14 @@ export default async function POST(req: NextRequest) {
 
     const lengthSeconds = parseInt(info.videoDetails.lengthSeconds);
 
-    return NextResponse.json({ duration: lengthSeconds });
+    return NextResponse.json(
+      { duration: lengthSeconds },
+      {
+        headers: {
+          "Set-Cookie": cookies,
+        },
+      }
+    );
   } catch (error) {
     console.error("Error fetching video info:", error);
     return NextResponse.json(
@@ -28,4 +35,4 @@ export default async function POST(req: NextRequest) {
       { status: 500 }
     );
   }
-}
+};
